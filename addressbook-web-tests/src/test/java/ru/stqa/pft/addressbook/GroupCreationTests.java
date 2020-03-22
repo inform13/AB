220defb94fa06f9e7a51079c05b66693a8e90b56
KaterinaY <inform13@bk.ru>
2020-03-22 16:04:25 +0300

    успешно xampp

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -16,24 +16,24 @@ import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
     FirefoxDriver wd;
-    
+
     @BeforeMethod
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
-    
+
     @Test
     public void GroupCreationTests() {
-        wd.get("http://addressbook/");
+        wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        new Actions(wd).doubleClick(wd.findElement(By.linkText("Группы"))).build().perform();
+        new Actions(wd).doubleClick(wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]"))).build().perform();
+        wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
@@ -46,13 +46,14 @@ public class GroupCreationTests {
         wd.findElement(By.name("group_footer")).sendKeys("test3");
         wd.findElement(By.name("submit")).click();
         wd.findElement(By.linkText("group page")).click();
+        wd.findElement(By.linkText("home")).click();
     }
-    
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
     }
-    
+
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
             wd.switchTo().alert();
