fdee1210870d1eec1cd91eaea2ab5eb17411ca00
KaterinaY <inform13@bk.ru>
2020-04-09 09:54:18 +0300

    2_11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -4,14 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class SessionHelper {
-  private FirefoxDriver wd;
+public class SessionHelper extends HelperBase {
 
   public SessionHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String username, String password) {
+    type (By.name("user"), username);
+    type (By.name("user"), password);
+
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
