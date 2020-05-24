8daf461bc57ed2f6d42ca9309a0065b3bd782ceb
KaterinaY <inform13@bk.ru>
2020-05-24 15:49:03 +0300

    контакты_4-3 4-4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,15 +4,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTests extends TestBase{
 
     @Test
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
         int before = app.getContactHelper().getContactCount();
+        //List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().createContact(new ContactData("Ekaterina", "Karikova", "karikova_e@mail.ru", "test1"), true );
         int after = app.getContactHelper().getContactCount();
+        //List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after, before + 1);
+        //Assert.assertEquals(after.size(), before.size() + 1);
+
     }
 
 }
